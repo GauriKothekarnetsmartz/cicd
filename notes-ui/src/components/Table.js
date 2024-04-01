6359496bef0d64992ae18b8fef1811b1c59a8188
GauriKothekarnetsmartz <160697991+GauriKothekarnetsmartz@users.noreply.github.com>
2024-04-01 18:55:49 +0530

    Update Table.js

--- notes-ui/src/components/Table.js
+++ notes-ui/src/components/Table.js
@@ -20,7 +20,7 @@ class Table extends Component {
 
     componentDidMount() {
         this.setState({ isLoading: true })
-        let api_url = 'http://51.20.4.62:4006/notes';
+        let api_url = 'http://localhost:4006/notes';
         fetch(api_url)
             .then(res => {
                 if(res.status >= 400) {
