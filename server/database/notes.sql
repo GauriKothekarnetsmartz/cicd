fa9a1a755156399506176ff8478a9263ae97c00c
GauriKothekarnetsmartz <160697991+GauriKothekarnetsmartz@users.noreply.github.com>
2024-05-16 12:21:35 +0530

    Update notes.sql

--- server/database/notes.sql
+++ server/database/notes.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `notes`
 --
-CREATE DATABASE IF NOT EXISTS `notes`;
+-- CREATE DATABASE IF NOT EXISTS `notes`;
 DROP TABLE IF EXISTS `notes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
